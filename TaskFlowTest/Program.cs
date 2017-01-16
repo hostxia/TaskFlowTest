9d8ea2a42149eddd60ed53e5bd79f6dcca2a6d52
Xiameng <Xiameng@PC-XIAMENG>
2017-01-16 20:32:44 +0800

    ABC

--- TaskFlowTest/Program.cs
+++ TaskFlowTest/Program.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using DevExpress.Xpo.DB;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DevExpress.Xpo;
 
 namespace TaskFlowTest
 {
@@ -14,8 +17,10 @@ namespace TaskFlowTest
         [STAThread]
         static void Main()
         {
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            XpoDefault.DataLayer = XpoDefault.GetDataLayer(ConfigurationManager.ConnectionStrings["Connection"].ConnectionString, AutoCreateOption.None);
             Application.Run(new XFrmMain());
         }
     }
--- TaskFlowTest/XFrmMain.Designer.cs
+++ TaskFlowTest/XFrmMain.Designer.cs
@@ -46,14 +46,14 @@
             this.layoutControlGroup2 = new DevExpress.XtraLayout.LayoutControlGroup();
             this.layoutControlItem2 = new DevExpress.XtraLayout.LayoutControlItem();
             this.layoutControlItem3 = new DevExpress.XtraLayout.LayoutControlItem();
-            this.layoutControlGroup3 = new DevExpress.XtraLayout.LayoutControlGroup();
-            this.layoutControlItem4 = new DevExpress.XtraLayout.LayoutControlItem();
             this.layoutControlItem5 = new DevExpress.XtraLayout.LayoutControlItem();
-            this.layoutControlItem6 = new DevExpress.XtraLayout.LayoutControlItem();
             this.layoutControlGroup4 = new DevExpress.XtraLayout.LayoutControlGroup();
             this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
             this.layoutControlItem7 = new DevExpress.XtraLayout.LayoutControlItem();
             this.emptySpaceItem1 = new DevExpress.XtraLayout.EmptySpaceItem();
+            this.layoutControlGroup3 = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.layoutControlItem6 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.layoutControlItem4 = new DevExpress.XtraLayout.LayoutControlItem();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
             this.layoutControl1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.checkEdit1.Properties)).BeginInit();
@@ -67,14 +67,14 @@
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem5)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem6)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem7)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem4)).BeginInit();
             this.SuspendLayout();
             // 
             // layoutControl1
@@ -90,13 +90,13 @@
             this.layoutControl1.Location = new System.Drawing.Point(0, 0);
             this.layoutControl1.Name = "layoutControl1";
             this.layoutControl1.Root = this.layoutControlGroup1;
-            this.layoutControl1.Size = new System.Drawing.Size(760, 493);
+            this.layoutControl1.Size = new System.Drawing.Size(715, 493);
             this.layoutControl1.TabIndex = 0;
             this.layoutControl1.Text = "layoutControl1";
             // 
             // xsbTest
             // 
-            this.xsbTest.Location = new System.Drawing.Point(661, 459);
+            this.xsbTest.Location = new System.Drawing.Point(616, 459);
             this.xsbTest.Name = "xsbTest";
             this.xsbTest.Size = new System.Drawing.Size(87, 22);
             this.xsbTest.StyleController = this.layoutControl1;
@@ -106,48 +106,48 @@
             // 
             // checkEdit1
             // 
-            this.checkEdit1.Location = new System.Drawing.Point(24, 110);
+            this.checkEdit1.Location = new System.Drawing.Point(24, 43);
             this.checkEdit1.Name = "checkEdit1";
             this.checkEdit1.Properties.Caption = "检查自定义条件";
-            this.checkEdit1.Size = new System.Drawing.Size(712, 19);
+            this.checkEdit1.Size = new System.Drawing.Size(331, 19);
             this.checkEdit1.StyleController = this.layoutControl1;
             this.checkEdit1.TabIndex = 7;
             // 
             // xchkSimulation
             // 
-            this.xchkSimulation.Location = new System.Drawing.Point(382, 133);
+            this.xchkSimulation.Location = new System.Drawing.Point(24, 109);
             this.xchkSimulation.Name = "xchkSimulation";
             this.xchkSimulation.Properties.Caption = "模拟执行流程";
-            this.xchkSimulation.Size = new System.Drawing.Size(354, 19);
+            this.xchkSimulation.Size = new System.Drawing.Size(667, 19);
             this.xchkSimulation.StyleController = this.layoutControl1;
             this.xchkSimulation.TabIndex = 6;
             // 
             // xchkCondition
             // 
-            this.xchkCondition.Location = new System.Drawing.Point(24, 133);
+            this.xchkCondition.Location = new System.Drawing.Point(359, 43);
             this.xchkCondition.Name = "xchkCondition";
             this.xchkCondition.Properties.Caption = "检查条件合法性";
-            this.xchkCondition.Size = new System.Drawing.Size(354, 19);
+            this.xchkCondition.Size = new System.Drawing.Size(332, 19);
             this.xchkCondition.StyleController = this.layoutControl1;
             this.xchkCondition.TabIndex = 5;
             // 
             // xchkAllTaskChain
             // 
-            this.xchkAllTaskChain.Location = new System.Drawing.Point(382, 43);
+            this.xchkAllTaskChain.Location = new System.Drawing.Point(360, 132);
             this.xchkAllTaskChain.Name = "xchkAllTaskChain";
             this.xchkAllTaskChain.Properties.Caption = "检查所有任务链";
-            this.xchkAllTaskChain.Size = new System.Drawing.Size(354, 19);
+            this.xchkAllTaskChain.Size = new System.Drawing.Size(331, 19);
             this.xchkAllTaskChain.StyleController = this.layoutControl1;
             this.xchkAllTaskChain.TabIndex = 4;
             // 
             // xbeTaskChain
             // 
-            this.xbeTaskChain.Location = new System.Drawing.Point(75, 43);
+            this.xbeTaskChain.Location = new System.Drawing.Point(75, 132);
             this.xbeTaskChain.Name = "xbeTaskChain";
             this.xbeTaskChain.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
             new DevExpress.XtraEditors.Controls.EditorButton()});
             this.xbeTaskChain.Properties.ReadOnly = true;
-            this.xbeTaskChain.Size = new System.Drawing.Size(303, 20);
+            this.xbeTaskChain.Size = new System.Drawing.Size(281, 20);
             this.xbeTaskChain.StyleController = this.layoutControl1;
             this.xbeTaskChain.TabIndex = 1;
             this.xbeTaskChain.ButtonClick += new DevExpress.XtraEditors.Controls.ButtonPressedEventHandler(this.xbeTaskChain_ButtonClick);
@@ -157,7 +157,7 @@
             this.xgridResult.Location = new System.Drawing.Point(24, 199);
             this.xgridResult.MainView = this.gridView1;
             this.xgridResult.Name = "xgridResult";
-            this.xgridResult.Size = new System.Drawing.Size(712, 244);
+            this.xgridResult.Size = new System.Drawing.Size(667, 244);
             this.xgridResult.TabIndex = 1;
             this.xgridResult.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
             this.gridView1});
@@ -220,80 +220,52 @@
             this.layoutControlGroup1.GroupBordersVisible = false;
             this.layoutControlGroup1.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
             this.layoutControlGroup2,
-            this.layoutControlGroup3,
             this.layoutControlGroup4,
             this.layoutControlItem7,
-            this.emptySpaceItem1});
+            this.emptySpaceItem1,
+            this.layoutControlGroup3});
             this.layoutControlGroup1.Location = new System.Drawing.Point(0, 0);
             this.layoutControlGroup1.Name = "Root";
-            this.layoutControlGroup1.Size = new System.Drawing.Size(760, 493);
+            this.layoutControlGroup1.Size = new System.Drawing.Size(715, 493);
             this.layoutControlGroup1.TextVisible = false;
             // 
             // layoutControlGroup2
             // 
             this.layoutControlGroup2.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
             this.layoutControlItem2,
-            this.layoutControlItem3});
-            this.layoutControlGroup2.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlItem3,
+            this.layoutControlItem5});
+            this.layoutControlGroup2.Location = new System.Drawing.Point(0, 66);
             this.layoutControlGroup2.Name = "layoutControlGroup2";
-            this.layoutControlGroup2.Size = new System.Drawing.Size(740, 67);
-            this.layoutControlGroup2.Text = "检查范围";
+            this.layoutControlGroup2.Size = new System.Drawing.Size(695, 90);
+            this.layoutControlGroup2.Text = "模拟流程";
             // 
             // layoutControlItem2
             // 
             this.layoutControlItem2.Control = this.xbeTaskChain;
-            this.layoutControlItem2.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlItem2.Location = new System.Drawing.Point(0, 23);
             this.layoutControlItem2.Name = "layoutControlItem2";
-            this.layoutControlItem2.Size = new System.Drawing.Size(358, 24);
+            this.layoutControlItem2.Size = new System.Drawing.Size(336, 24);
             this.layoutControlItem2.Text = "任务链：";
             this.layoutControlItem2.TextSize = new System.Drawing.Size(48, 14);
             // 
             // layoutControlItem3
             // 
             this.layoutControlItem3.Control = this.xchkAllTaskChain;
-            this.layoutControlItem3.Location = new System.Drawing.Point(358, 0);
+            this.layoutControlItem3.Location = new System.Drawing.Point(336, 23);
             this.layoutControlItem3.Name = "layoutControlItem3";
-            this.layoutControlItem3.Size = new System.Drawing.Size(358, 24);
+            this.layoutControlItem3.Size = new System.Drawing.Size(335, 24);
             this.layoutControlItem3.TextSize = new System.Drawing.Size(0, 0);
             this.layoutControlItem3.TextVisible = false;
-            // 
-            // layoutControlGroup3
-            // 
-            this.layoutControlGroup3.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
-            this.layoutControlItem4,
-            this.layoutControlItem5,
-            this.layoutControlItem6});
-            this.layoutControlGroup3.Location = new System.Drawing.Point(0, 67);
-            this.layoutControlGroup3.Name = "layoutControlGroup3";
-            this.layoutControlGroup3.Size = new System.Drawing.Size(740, 89);
-            this.layoutControlGroup3.Text = "检查项";
-            // 
-            // layoutControlItem4
-            // 
-            this.layoutControlItem4.Control = this.xchkCondition;
-            this.layoutControlItem4.Location = new System.Drawing.Point(0, 23);
-            this.layoutControlItem4.Name = "layoutControlItem4";
-            this.layoutControlItem4.Size = new System.Drawing.Size(358, 23);
-            this.layoutControlItem4.TextSize = new System.Drawing.Size(0, 0);
-            this.layoutControlItem4.TextVisible = false;
             // 
             // layoutControlItem5
             // 
             this.layoutControlItem5.Control = this.xchkSimulation;
-            this.layoutControlItem5.Location = new System.Drawing.Point(358, 23);
+            this.layoutControlItem5.Location = new System.Drawing.Point(0, 0);
             this.layoutControlItem5.Name = "layoutControlItem5";
-            this.layoutControlItem5.Size = new System.Drawing.Size(358, 23);
+            this.layoutControlItem5.Size = new System.Drawing.Size(671, 23);
             this.layoutControlItem5.TextSize = new System.Drawing.Size(0, 0);
             this.layoutControlItem5.TextVisible = false;
-            // 
-            // layoutControlItem6
-            // 
-            this.layoutControlItem6.Control = this.checkEdit1;
-            this.layoutControlItem6.Location = new System.Drawing.Point(0, 0);
-            this.layoutControlItem6.Name = "layoutControlItem6";
-            this.layoutControlItem6.Size = new System.Drawing.Size(716, 23);
-            this.layoutControlItem6.TextSize = new System.Drawing.Size(0, 0);
-            this.layoutControlItem6.TextVisible = false;
             // 
             // layoutControlGroup4
             // 
@@ -301,15 +273,15 @@
             this.layoutControlItem1});
             this.layoutControlGroup4.Location = new System.Drawing.Point(0, 156);
             this.layoutControlGroup4.Name = "layoutControlGroup4";
-            this.layoutControlGroup4.Size = new System.Drawing.Size(740, 291);
-            this.layoutControlGroup4.Text = "检查结果";
+            this.layoutControlGroup4.Size = new System.Drawing.Size(695, 291);
+            this.layoutControlGroup4.Text = "结果";
             // 
             // layoutControlItem1
             // 
             this.layoutControlItem1.Control = this.xgridResult;
             this.layoutControlItem1.Location = new System.Drawing.Point(0, 0);
             this.layoutControlItem1.Name = "layoutControlItem1";
-            this.layoutControlItem1.Size = new System.Drawing.Size(716, 248);
+            this.layoutControlItem1.Size = new System.Drawing.Size(671, 248);
             this.layoutControlItem1.Text = "执行结果：";
             this.layoutControlItem1.TextSize = new System.Drawing.Size(0, 0);
             this.layoutControlItem1.TextVisible = false;
@@ -317,7 +289,7 @@
             // layoutControlItem7
             // 
             this.layoutControlItem7.Control = this.xsbTest;
-            this.layoutControlItem7.Location = new System.Drawing.Point(649, 447);
+            this.layoutControlItem7.Location = new System.Drawing.Point(604, 447);
             this.layoutControlItem7.MaxSize = new System.Drawing.Size(91, 26);
             this.layoutControlItem7.MinSize = new System.Drawing.Size(91, 26);
             this.layoutControlItem7.Name = "layoutControlItem7";
@@ -331,14 +303,42 @@
             this.emptySpaceItem1.AllowHotTrack = false;
             this.emptySpaceItem1.Location = new System.Drawing.Point(0, 447);
             this.emptySpaceItem1.Name = "emptySpaceItem1";
-            this.emptySpaceItem1.Size = new System.Drawing.Size(649, 26);
+            this.emptySpaceItem1.Size = new System.Drawing.Size(604, 26);
             this.emptySpaceItem1.TextSize = new System.Drawing.Size(0, 0);
+            // 
+            // layoutControlGroup3
+            // 
+            this.layoutControlGroup3.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.layoutControlItem6,
+            this.layoutControlItem4});
+            this.layoutControlGroup3.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlGroup3.Name = "layoutControlGroup3";
+            this.layoutControlGroup3.Size = new System.Drawing.Size(695, 66);
+            this.layoutControlGroup3.Text = "检查项";
+            // 
+            // layoutControlItem6
+            // 
+            this.layoutControlItem6.Control = this.checkEdit1;
+            this.layoutControlItem6.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlItem6.Name = "layoutControlItem6";
+            this.layoutControlItem6.Size = new System.Drawing.Size(335, 23);
+            this.layoutControlItem6.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem6.TextVisible = false;
+            // 
+            // layoutControlItem4
+            // 
+            this.layoutControlItem4.Control = this.xchkCondition;
+            this.layoutControlItem4.Location = new System.Drawing.Point(335, 0);
+            this.layoutControlItem4.Name = "layoutControlItem4";
+            this.layoutControlItem4.Size = new System.Drawing.Size(336, 23);
+            this.layoutControlItem4.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem4.TextVisible = false;
             // 
             // XFrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(760, 493);
+            this.ClientSize = new System.Drawing.Size(715, 493);
             this.Controls.Add(this.layoutControl1);
             this.Name = "XFrmMain";
             this.Text = "任务链测试工具";
@@ -356,14 +356,14 @@
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem5)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem6)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem7)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem4)).EndInit();
             this.ResumeLayout(false);
 
         }
